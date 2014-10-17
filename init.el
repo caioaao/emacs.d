95834c382a5f4a20d634f23950f6242fc874083e
Caio <caioaao@gmail.com>
2014-10-17 16:55:34 -0300

    :D

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
+;; Adding package paths
+(add-to-list 'load-path "~/.emacs.d/elisp/")
+
 ;; Init packages before
 (setq package-enable-at-startup nil)
 (package-initialize)
@@ -9,6 +12,16 @@
 (setq coding-system-for-read 'utf-8)
 (setq coding-system-for-write 'utf-8)
 
-(add-to-list 'load-path "~/.emacs.d/elisp/")
+
+;; theme
+(load-theme 'wombat t)
+
+;; package managers
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+
+
 (setq auto-mode-alist (cons '("\\.\\(pde\\|ino\\)$" . arduino-mode) auto-mode-alist))
 (autoload 'arduino-mode "arduino-mode" "Arduino editing mode." t)

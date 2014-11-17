e30df51fa87a7de0c74ad3fdb1d5f46719367466
Caio <caioaao@gmail.com>
2014-11-17 18:25:32 +0000

    adicionando pacotes

---
+++ elpa/epl-20140823.609/epl-autoloads.el
@@ -0,0 +1,15 @@
+;;; epl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("epl.el") (21610 15560 421604 705000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; epl-autoloads.el ends here

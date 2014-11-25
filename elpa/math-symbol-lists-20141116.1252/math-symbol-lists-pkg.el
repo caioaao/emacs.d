1b58f79849289b62cbdcfdf0684c9d01a6ce00a1
Caio <caioaao@gmail.com>
2014-11-25 18:39:13 +0000

    adicionando AC do auctex e ac-math

---
+++ elpa/math-symbol-lists-20141116.1252/math-symbol-lists-pkg.el
@@ -0,0 +1,1 @@
+(define-package "math-symbol-lists" "20141116.1252" "Lists of Unicode mathematical symbols and latex commands" 'nil :url "https://github.com/vspinu/math-symbol-lists" :keywords '("unicode" "symbols" "mathematics"))

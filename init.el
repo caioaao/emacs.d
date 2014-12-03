92dfce79ab7f69b09a64a255b52b01aa7bf57498
Caio <caioaao@gmail.com>
2014-12-03 12:07:21 +0000

    config de IDE p python

--- init.el
+++ init.el
@@ -154,35 +154,51 @@
 (set-variable (quote tex-dvi-view-command) "evince")
 
 
-;; org-trello
-(require 'org-trello)
+;; virtualenvwrapper
+(require 'virtualenvwrapper)
+(venv-initialize-interactive-shells) ;; if you want interactive shell support
+(venv-initialize-eshell) ;; if you want eshell support
+(setq venv-location "~/my/dev/venvs/")
 
 ;; pylint
 ;; Configure flymake for Python
-(setq pylint "epylint")
-(when (load "flymake" t)
-  (defun flymake-pylint-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list (expand-file-name pylint "") (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pylint-init)))
-
-;; Set as a minor mode for Python
-(add-hook 'python-mode-hook '(lambda () (flymake-mode)))
 
 ;; Autocomplete minor mode
 (add-hook 'python-mode-hook '(lambda() (ac-python)))
-
 
 ;; Auto-insert mode
 (auto-insert-mode)
 (setq auto-insert-directory "~/.emacs.d/templates/")
 (setq auto-insert-query nil)
 (define-auto-insert "\.py" "python-template.py")
+
+;; jedi.el - python autocomplete
+(require 'jedi)
+
+(setq jedi:complete-on-dot t)
+(add-hook 'python-mode-hook 'jedi:setup)
+
+;; ipython
+(require 'python-mode)
+
+(setq
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args ""
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+   "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+   "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
+
+;; limitar tamanho das colunas
+(require 'fill-column-indicator)
+(define-globalized-minor-mode
+ global-fci-mode fci-mode (lambda () (fci-mode 1)))
+(global-fci-mode t)
 
 (provide 'init)
 ;;; init.el ends here

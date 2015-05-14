d893a5deb2a5072f26d3d7ea504c9b3cbfb2d006
Caio <caioaao@gmail.com>
2015-05-14 02:29:34 -0300

    organizing init.el and fixing delayed boot

--- init.el
+++ init.el
@@ -5,27 +5,52 @@
 
 ;;; Code:
 
+;; Some vars
+
+(defvar bin-folder "~/.emacs.d/bin")
+(defvar elisp-folder "~/.emacs.d/elisp")
+
+(defvar local-root-folder "~/.emacs.d/local")
+(defvar local-bin-folder (concat local-root-folder "/bin"))
+(defvar local-elisp-folder (concat local-root-folder "/elisp"))
+
+
+
 ;; Creating folders
-(when (not (file-exists-p "~/.emacs.d/bin"))
-  (make-directory "~/.emacs.d/bin"))
-(when (not (file-exists-p "~/.emacs.d/local"))
-  (make-directory "~/.emacs.d/local"))
-(when (not (file-exists-p "~/.emacs.d/local/bin"))
-  (make-directory "~/.emacs.d/local/bin"))
+(when (not (file-exists-p bin-folder))
+  (make-directory bin-folder))
+
+(when (not (file-exists-p local-root-folder))
+  (make-directory local-root-folder))
+(when (not (file-exists-p local-elisp-folder))
+  (make-directory local-elisp-folder))
+(when (not (file-exists-p local-bin-folder))
+  (make-directory local-bin-folder))
+
 
 
 ;; Adding package paths
-(add-to-list 'load-path "~/.emacs.d/elisp/")
-(add-to-list 'load-path "~/.emacs.d/local/elisp/")
+(add-to-list 'load-path elisp-folder)
+(add-to-list 'load-path local-elisp-folder)
+
 
 
 ;; Getting local config file
-(when (file-exists-p "~/.emacs.d/local/local-init.el")
+(when (file-exists-p (concat local-root-folder "/local-init.el"))
   (require 'local-init))
+
+
 
 ;; Init packages before
 (setq package-enable-at-startup nil)
 (package-initialize)
+
+
+
+;; windows utility functions (required for setting PATH)
+(require 'win32-utils)
+
+
 
 ;; Fixing autosave/backup files
 (setq backup-directory-alist
@@ -33,10 +58,21 @@
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
+
+
 ;; Adding a bin folders
 (setq exec-path
-      (append '("~/.emacs.d/local/bin"
-                "~/.emacs.d/bin") exec-path))
+      (append '(local-bin-folder
+                bin-folder) exec-path))
+
+(when (boundp 'using-win32)
+  (setenv "PATH" (concat
+                  (w32utils-convert-to-std-path bin-folder) ";"
+                  (w32utils-convert-to-std-path local-bin-folder) ";"
+                  (getenv "PATH"))))
+                                                              
+
+
 
 ;; Indent using spaces only
 (setq-default indent-tabs-mode nil)
@@ -44,21 +80,24 @@
 (setq-default c-basic-offset 4)
 
 
+
 ;; markdown mode
 (require 'markdown-mode)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
-;; :D
+
+
+;; C/C++ mode
 (require 'cc-mode)
 
-;; git mode
-(require 'magit)
-(setq magit-last-seen-setup-instructions "1.4.0") ;; really?
+
 
 ;; For pt-Br dead keys to work
 (set-input-mode nil nil 1)
 (require 'iso-transl)
+
+
 
 ;; Set encoding
 (setenv "LC_CTYPE" "UTF-8")
@@ -66,31 +105,27 @@
 (setq coding-system-for-read 'utf-8)
 (setq coding-system-for-write 'utf-8)
 
+
+
 ;; package managers
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("marmalade" . "http://marmalade-repo.org/packages/")
+        ("melpa" . "http://melpa.milkbox.net/packages/")))
 
-;; theme
-(load-theme 'wombat t)
 
-;; Remove toolbar
-(tool-bar-mode -1)
 
-;; Auto linum-mode
-(global-linum-mode 1)
+(require 'guicfg)
 
-;; No initial screen
-(setq-default inhibit-startup-screen t)
-
-;; Clear scratch buffer
-(setq-default initial-scratch-message nil)
 
 ;; yes or no becomes y or n
 (defalias 'yes-or-no-p 'y-or-n-p)
 
+
+
 ;; yasnippet
-;; should be loaded before auto complete so that they can work together
+; (should be loaded before auto complete so that they can work
+; together)
 (require 'yasnippet)
 (yas-global-mode 1)
 
@@ -101,66 +136,32 @@
 (setq yas-snippet-dirs (append yas-snippet-dirs
                                '("~/.emacs.d/snippets")))
 
+
+
 ;; auto complete mode
 ;; should be loaded after yasnippet so that they can work together
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 (ac-config-default)
 
-;; set the trigger key so that it can work together with yasnippet on tab key,
-;; if the word exists in yasnippet, pressing tab will cause yasnippet to
-;; activate, otherwise, auto-complete will
+;; set the trigger key so that it can work together with yasnippet on
+;; tab key, if the word exists in yasnippet, pressing tab will cause
+;; yasnippet to activate, otherwise, auto-complete will
 (ac-set-trigger-key "TAB")
 (ac-set-trigger-key "<tab>")
 
-;; auto-complete-clang
-(require 'auto-complete-clang)
-;; (define-key c-mode-base-map [(control tab)] 'ac-complete-clang) -> DEFEITO
 
 
 ;; flycheck
+(require 'flymake)
+(require 'flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
-;; org-mode
-(require 'org)
-
-(setq my-org-files-dir "~/.emacs.d/orgfiles/")
-(setq my-org-agenda-dir (concat my-org-files-dir "agenda/"))
-
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
-(setq  org-return-follows-link t)
-
-;; initial org folders setup
-(when (not (file-exists-p my-org-files-dir)) (make-directory my-org-files-dir))
-(when (not (file-exists-p my-org-agenda-dir)) (make-directory my-org-agenda-dir))
-
-;; Snippet to collect all .org from my Org directory and subdirs
-(setq org-agenda-file-regexp "\\`[^.].*\\.org\\'") ; default value
-(defun load-org-agenda-files-recursively (dir) "Find all directories in DIR."
-    (unless (file-directory-p dir) (error "Not a directory `%s'" dir))
-    (unless (equal (directory-files dir nil org-agenda-file-regexp t) nil)
-      (add-to-list 'org-agenda-files dir)
-    )
-    (dolist (file (directory-files dir nil nil t))
-        (unless (member file '("." ".."))
-            (let ((file (concat dir file "/")))
-                (when (file-directory-p file)
-                    (load-org-agenda-files-recursively file)
-                )
-            )
-        )
-    )
-    )
 
 
-(load-org-agenda-files-recursively my-org-agenda-dir ) ; trailing slash required
+;; org mode
+(require 'orgcfg)
 
-
-;; tramp-mode: remote connections
-(require 'tramp)
-(setq tramp-default-method "ssh")
 
 
 ;; window resizing shortcuts
@@ -172,60 +173,28 @@
 			   ("<down>" . 'enlarge-window)))
 
 
-;; force latex to use pdflatex
+
+;; latex
+; force latex to use pdflatex
 (set-variable (quote latex-run-command) "pdflatex")
 (set-variable (quote tex-dvi-view-command) "evince")
 
-
-;; add latex mode to auto-complete
+; add latex mode to auto-complete
 (add-to-list 'ac-modes 'latex-mode)
 
-;; virtualenvwrapper
-(require 'virtualenvwrapper)
-(venv-initialize-interactive-shells) ;; if you want interactive shell support
-(venv-initialize-eshell) ;; if you want eshell support
-(setq venv-location "~/my/dev/venvs/")
 
-;; pylint
-;; Configure flymake for Python
-
-;; Autocomplete minor mode
-(require 'ac-python)
-(add-hook 'python-mode-hook '(lambda() (ac-python)))
 
 ;; Auto-insert mode
 (auto-insert-mode)
 (setq auto-insert-directory "~/.emacs.d/templates/")
 (setq auto-insert-query nil)
-(define-auto-insert "\.py" "python-template.py")
-(define-auto-insert "sol\.cpp" "competitive-template.cpp")
 
-;; jedi.el - python autocomplete
-(require 'jedi)
 
-(setq jedi:complete-on-dot t)
-(add-hook 'python-mode-hook 'jedi:setup)
 
-;; ipython
-(require 'python-mode)
+;; Python config
+(require 'pycfg)
 
-(setq
- python-shell-interpreter "ipython"
- python-shell-interpreter-args ""
- python-shell-prompt-regexp "In \\[[0-9]+\\]: "
- python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
- python-shell-completion-setup-code
-   "from IPython.core.completerlib import module_completion"
- python-shell-completion-module-string-code
-   "';'.join(module_completion('''%s'''))\n"
- python-shell-completion-string-code
-   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
 
-;; limitar tamanho das colunas
-(require 'fill-column-indicator)
-(define-globalized-minor-mode
- global-fci-mode fci-mode (lambda () (fci-mode 1)))
-(global-fci-mode t)
 
 ;; web-mode (html, css, javascript)
 (require 'web-mode)
@@ -234,11 +203,23 @@
 (require 'ac-html)
 (add-to-list 'ac-modes 'web-mode)
 
+
+
+;; C/C++ config
 ;; cc-mode
 (require 'cc-mode)
+
 ;; sets extended mode curly braces as default
 (setq c-default-style "linux"
       c-basic-offset 4)
+;; C++11 as standard
+(add-hook 'c++-mode-hook
+          (lambda () (setq flycheck-gcc-language-standard "c++11")))
+;; auto complete
+(require 'auto-complete-clang)
+(define-auto-insert "sol\.cpp$" "competitive-template.cpp")
+
+
 
 ;; ledger-mode (accounting)
 (require 'ledger-mode)
@@ -246,14 +227,8 @@
 (require 'ledger-helper)
 
 
-(require 'flymake)
-(require 'flycheck)
-
-(add-hook 'c++-mode-hook
-          (lambda () (setq flycheck-gcc-language-standard "c++11")))
-
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
 
 (provide 'init)
 ;;; init.el ends here
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)

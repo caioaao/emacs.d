dc73a2a03d50298202f8bc1bd5c32af22ea2574d
Caio <caioaao@gmail.com>
2015-06-08 23:38:01 -0300

    adding smartparens and golden-ratio

--- init.el
+++ init.el
@@ -226,6 +226,11 @@
 (require 'ledger-helper)
 
 
+;; smartparens
+(require 'smartparenscfg)
+
+
+
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)

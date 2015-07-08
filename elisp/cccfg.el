1f158bae6a7c0e6a07d2daa9f8a2d7b81897b73a
coliveira <coliveira@muxi.com.br>
2015-07-08 12:11:11 -0300

    adding command to resolve subst-based paths

--- elisp/cccfg.el
+++ elisp/cccfg.el
@@ -27,7 +27,7 @@
 ;; cc-mode
 (require 'cc-mode)
 
-;;
+;; Adding newline after files
 (add-hook 'c-c++-mode-hook
           (lambda () (setq require-final-newline t)))
 
@@ -40,6 +40,38 @@
 ;; auto complete
 ;; (require 'auto-complete-clang)
 (define-auto-insert "sol\.cpp$" "competitive-template.cpp")
+
+;; ggtags
+(require 'ggtags)
+
+(setq ggtags-oversize-limit (* 1 1024 1024))
+
+
+(add-hook 'dired-mode-hook (lambda () (ggtags-mode 1)))
+(add-hook 'c-mode-hook (lambda () (ggtags-mode 1)))
+(add-hook 'c++-mode-hook (lambda () (ggtags-mode 1)))
+(add-hook 'java-mode-hook (lambda () (ggtags-mode 1)))
+
+
+(define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
+(define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
+(define-key ggtags-mode-map (kbd "C-c g r") 'ggtags-find-reference)
+(define-key ggtags-mode-map (kbd "C-c g f") 'ggtags-find-file)
+(define-key ggtags-mode-map (kbd "C-c g c") 'ggtags-create-tags)
+(define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
+
+(define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark)
+
+(require 'cc-mode)
+(require 'semantic)
+
+(global-semanticdb-minor-mode 1)
+(global-semantic-idle-scheduler-mode 1)
+
+(semantic-mode 1)
+
+(require 'company)
+(global-company-mode 1)
 
 (provide 'cccfg)
 ;;; cccfg.el ends here
--- elisp/win32-utils.el
+++ elisp/win32-utils.el
@@ -18,21 +18,55 @@
 
 ;;; Commentary:
 
-;;
-
 ;;; Code:
 
 (defun w32utils-convert-to-std-path (oldpath)
   "Convert OLDPATH to win32 standard path, when in win32 environment."
   (convert-standard-filename (expand-file-name oldpath)))
 
-(defun w32utils-find-original-from-subst (drive)
-  "DRIVE format: 'X:'."
-  (with-temp-buffer
-    (call-process "subst" nil t)
-    (goto-char (point-min))
-    (when (re-search-forward (format "^%s\\\\: => \\(.*\\)$" drive) nil t)
-      (match-string 1))))
+(defun w32utils-find-original-path (drive)
+  "Find original path for drive, if it belongs to subst.  DRIVE format: 'X:'."
+  (let ((path nil))
+    (with-temp-buffer
+      (call-process "subst" nil t)
+      (goto-char (point-min))
+      (when (re-search-forward (format "^%s\\\\: => \\(.*\\)$" drive) nil t)
+        (setq path (match-string 1))))
+    (when (not path)
+      (setq path drive))
+    path))
+
+(defun w32utils-expand-subst (path)
+  "Expand PATH to original, if contained in SUBST."
+  (if (string-match "^.:" path)
+    (let ((original-drive-path
+           (w32utils-find-original-path (match-string 0 path))))
+      (string-match "^.:" path)
+      (replace-match original-drive-path nil t path))
+    path))
+
+(defun w32utils-find-file-subst (filename &optional wildcards)
+  "Expand the subst drive before opening FILENAME.
+WILDCARDS has the same effect as the original \"find-file\" function."
+  (interactive "FFilename: ")
+  (find-file (w32utils-expand-subst filename) wildcards))
+
+(defvar w32utils-mode-keymap (make-sparse-keymap) "Keymap for pw-mode.")
+(define-key w32utils-mode-keymap (kbd "<open>") 'w32utils-find-file-subst)
+(define-key w32utils-mode-keymap (kbd "C-x C-f") 'w32utils-find-file-subst)
+(define-key w32utils-mode-keymap (kbd "<menu-bar>") 'w32utils-find-file-subst)
+(define-key w32utils-mode-keymap (kbd "<file>") 'w32utils-find-file-subst)
+(define-key w32utils-mode-keymap (kbd "<new-file>") 'w32utils-find-file-subst)
+
+(define-minor-mode w32utils-mode "Minor mode for Windows"
+  :lighter " w32utils" :group 'w32utils
+  :keymap w32utils-mode-keymap)
+
+(when (or
+       (eq system-type 'ms-dos)
+       (eq system-type 'windows-nt)
+       (eq system-type 'cygwin))
+  (w32utils-mode 1))
 
 (provide 'win32-utils)
 ;;; win32-utils.el ends here

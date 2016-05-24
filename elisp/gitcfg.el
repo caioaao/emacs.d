9b0ed3ed4f799b46d58dffba39627e92c79ee935
Caio Oliveira <caio.oliveira@nubank.com.br>
2016-05-24 12:48:53 -0300

    Altered magit conf
    `magit-status` buffer now appears on current buffer. Some things are
    still buggy (when `magit-commit` is called, the diff appears in
    `current-buffer`, hiding the commit message buffer).

--- elisp/gitcfg.el
+++ elisp/gitcfg.el
@@ -32,5 +32,19 @@
 (global-set-key (kbd "C-c m s") 'magit-status)
 (global-set-key (kbd "C-c m b") 'magit-blame)
 
+;; See https://github.com/magit/magit/issues/2541
+(setq magit-display-buffer-function
+      (lambda (buffer)
+        (display-buffer
+         buffer (if (and (derived-mode-p 'magit-mode)
+                         (memq (with-current-buffer buffer major-mode)
+                               '(magit-process-mode
+                                 magit-revision-mode
+                                 magit-diff-mode
+                                 magit-stash-mode
+                                 magit-status-mode)))
+                    nil
+                  '(display-buffer-same-window)))))
+
 (provide 'gitcfg)
 ;;; gitcfg.el ends here

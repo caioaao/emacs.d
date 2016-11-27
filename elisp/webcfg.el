79ce33956710206b1cc9a2391f94c1703956f636
caio <caioaao@gmail.com>
2016-11-27 00:13:17 -0200

    restclient for files with .http extension

--- elisp/webcfg.el
+++ elisp/webcfg.el
@@ -36,6 +36,8 @@
 (setq web-mode-enable-auto-closing t)
 (setq web-mode-enable-auto-pairing t)
 
+(add-to-list 'auto-mode-alist '("\\.http\\'" . restclient-mode))
+
 
 (provide 'webcfg)
 ;;; webcfg.el ends here

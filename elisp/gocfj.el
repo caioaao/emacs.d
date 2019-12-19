8ceaa6e1ba577ab86e1acefcbd16cf5eaeffe6df
Caio Oliveira <caioaao@gmail.com>
2019-12-18 19:38:50 -0500

    go stuff

---
+++ elisp/gocfj.el
@@ -0,0 +1,32 @@
+;;; gocfj.el --- golang config                       -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019
+
+;; Author:  <caiobrv@pop-os>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defun my-go-mode-hook ()
+  ; Godef jump key binding
+  (local-set-key (kbd "M-.") 'godef-jump))
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+
+(provide 'gocfj)
+;;; gocfj.el ends here

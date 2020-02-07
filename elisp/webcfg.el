485f57a1a1f5c06ec3f777842a93e18aa84fc27f
Caio Oliveira <caioaao@gmail.com>
2020-02-07 13:49:14 -0500

    the usual web indent

--- elisp/webcfg.el
+++ elisp/webcfg.el
@@ -38,7 +38,8 @@
   (progn
     (setq web-mode-enable-auto-closing t)
     (setq web-mode-enable-auto-pairing t)
-    (setq web-mode-code-indent-offset 2)))
+    (setq web-mode-code-indent-offset 2)
+    (setq web-mode-markup-indent-offset 2)))
 
 
 (use-package restclient

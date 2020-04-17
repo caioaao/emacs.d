a66ab9fc21aa11d32eeff6a47534878fe5080070
Caio Oliveira <caioaao@gmail.com>
2020-04-16 22:36:08 -0400

    dart

---
+++ elisp/dartcfg.el
@@ -0,0 +1,45 @@
+;;; dartcfg.el ---                                   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Caio Oliveira
+
+;; Author: Caio Oliveira
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(use-package dart-mode :ensure t)
+(use-package flutter
+  :ensure t
+  :after dart-mode
+  :bind (:map dart-mode-map
+              ("C-M-x" . #'flutter-run-or-hot-reload)))
+
+(use-package lsp-mode
+  :ensure t
+  :defines lsp-dart-sdk-dir
+  :hook
+  (dart-mode . lsp)
+  (before-save . lsp-format-buffer)
+  :config
+  (require 'lsp-dart)
+  (setq lsp-dart-sdk-dir "/opt/flutter/bin/cache/dart-sdk/"))
+
+(provide 'dartcfg)
+;;; dartcfg.el ends here

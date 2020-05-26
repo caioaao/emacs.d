e3fbd4d6dad0cd6a795dfe4113daec4359b2cfa8
Caio Oliveira <caioaao@gmail.com>
2020-05-26 17:44:07 -0400

    ts-comint commands + comment out clojure lsp

--- elisp/webcfg.el
+++ elisp/webcfg.el
@@ -96,5 +96,16 @@
   :config
   (add-to-list 'lsp-clients-typescript-server-args "--jsx"))
 
+(use-package ts-comint
+  :ensure t
+  :hook
+  (web-mode . (lambda ()
+                (when (string-match "tsx?\\'" (file-name-extension buffer-file-name))
+                  (local-set-key (kbd "C-x C-e") 'ts-send-last-sexp)
+                  (local-set-key (kbd "C-M-x") 'ts-send-last-sexp-and-go)
+                  (local-set-key (kbd "C-c C-k") 'ts-send-buffer)
+                  (local-set-key (kbd "C-c C-b") 'ts-send-buffer-and-go)
+                  (local-set-key (kbd "C-c l") 'ts-load-file-and-go)))))
+
 (provide 'webcfg)
 ;;; webcfg.el ends here

7c5b7ae421ce69c486f397f72f5c462c4821809c
Caio Oliveira <caioaao@gmail.com>
2021-02-17 10:11:42 -0300

    add flag to see if it changes anything

--- elisp/webcfg.el
+++ elisp/webcfg.el
@@ -53,7 +53,8 @@
   (setq web-mode-enable-auto-closing t)
   (setq web-mode-enable-auto-pairing t)
   (setq web-mode-code-indent-offset 2)
-  (setq web-mode-markup-indent-offset 2))
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-enable-literal-interpolation t))
 
 
 (use-package restclient

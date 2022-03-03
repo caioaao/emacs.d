11d97e53634873931acdd564f7d0b7b549505707
Caio Oliveira <caioaao@gmail.com>
2022-03-03 12:19:17 -0300

    automatic variables

--- init.el
+++ init.el
@@ -156,12 +156,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(helm-minibuffer-history-key "M-p")
  '(package-selected-packages
-   (quote
-    (cuda-mode evil-collection evil-mode evil-visual-mark-mode typescript-mode pseudocode-mode command-log-mode helm-xref quelpa quelpa-use-package reason-mode direnv nix-mode dockerfile-mode yaml-mode plantuml-mode ledger-mode terraform-mode protobuf-mode graphviz-dot-mode cmake-mode flutter dart-mode sql-indent go-mode lsp-java gradle-mode groovy-mode magit jupyter pipenv toml-mode python-pytest blacken py-isort racer rust-mode utop dune tuareg slime clojure-snippets clj-refactor cider clojure-mode ts-comint js2-mode restclient web-mode toc-org ox-gfm org-tree-slide spaceline powerline fic-mode rainbow-delimiters smartrep doom-themes lsp-ui company-lsp lsp-mode markdown-mode rainbow-mode which-key helm-ag helm-projectile projectile paredit company helpful flycheck avy anzu jsonnet-mode json-mode helm undo-tree yasnippet major-mode-hydra exec-path-from-shell diminish eglot use-package)))
- '(projectile-indexing-method (quote hybrid))
- '(safe-local-variable-values (quote ((create-lockfiles)))))
+   '(cuda-mode evil-collection evil-mode evil-visual-mark-mode typescript-mode pseudocode-mode command-log-mode helm-xref quelpa quelpa-use-package reason-mode direnv nix-mode dockerfile-mode yaml-mode plantuml-mode ledger-mode terraform-mode protobuf-mode graphviz-dot-mode cmake-mode flutter dart-mode sql-indent go-mode lsp-java gradle-mode groovy-mode magit jupyter pipenv toml-mode python-pytest blacken py-isort racer rust-mode utop dune tuareg slime clojure-snippets clj-refactor cider clojure-mode ts-comint js2-mode restclient web-mode toc-org ox-gfm org-tree-slide spaceline powerline fic-mode rainbow-delimiters smartrep doom-themes lsp-ui company-lsp lsp-mode markdown-mode rainbow-mode which-key helm-ag helm-projectile projectile paredit company helpful flycheck avy anzu jsonnet-mode json-mode helm undo-tree yasnippet major-mode-hydra exec-path-from-shell diminish eglot use-package))
+ '(projectile-indexing-method 'hybrid)
+ '(safe-local-variable-values '((create-lockfiles))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

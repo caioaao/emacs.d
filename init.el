d7724bb7057a9375f3990fdaef5c9d1f2e514186
Caio Oliveira <caioaao@gmail.com>
2022-03-06 22:22:28 -0300

    only require org-mode on init.el if tangling new config

--- init.el
+++ init.el
@@ -1,9 +1,8 @@
-(require 'org)
-
 (defvar user/config-org-file (concat user-emacs-directory "config.org"))
 (defvar user/config-el-file  (concat user-emacs-directory "config.el"))
 
 (when (file-newer-than-file-p user/config-org-file user/config-el-file)
+  (require 'org)
   (org-babel-tangle-file user/config-org-file)
   ;; TODO byte-compile is broken right now
   ;; (byte-compile-file user/config-el-file)

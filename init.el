fe5347d7cedc27dcc99ff825b67084a77beab0fa
Caio Oliveira <caioaao@gmail.com>
2023-07-10 11:52:19 -0300

    make it easier to reload config

--- init.el
+++ init.el
@@ -1,11 +1,14 @@
 (defvar user/config-org-file (concat user-emacs-directory "config.org"))
 (defvar user/config-el-file  (concat user-emacs-directory "config.el"))
 
-(when (file-newer-than-file-p user/config-org-file user/config-el-file)
-  (require 'org)
-  (org-babel-tangle-file user/config-org-file)
-  ;; TODO byte-compile is broken right now
-  ;; (byte-compile-file user/config-el-file)
-  )
+(defun user/reload-config ()
+  (interactive)
+  (when (file-newer-than-file-p user/config-org-file user/config-el-file)
+    (require 'org)
+    (org-babel-tangle-file user/config-org-file)
+    ;; TODO byte-compile is broken right now
+    ;; (byte-compile-file user/config-el-file)
+    )
+  (load-file user/config-el-file))
 
-(load-file user/config-el-file)
+(user/reload-config)

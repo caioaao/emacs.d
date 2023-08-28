98c469a63af85001e2fbc17307171bfbef79af1a
Caio Oliveira <caioaao@gmail.com>
2023-08-28 11:44:19 -0300

    fix tangle

--- init.el
+++ init.el
@@ -3,12 +3,12 @@
 
 (defun user/reload-config ()
   (interactive)
+  (when (file-exists-p user/config-el-file)
+      (load-file user/config-el-file))
   (when (file-newer-than-file-p user/config-org-file user/config-el-file)
     (require 'org)
-    (org-babel-tangle-file user/config-org-file)
-    ;; TODO byte-compile is broken right now
-    ;; (byte-compile-file user/config-el-file)
-    )
-  (load-file user/config-el-file))
+    (org-babel-tangle-file user/config-org-file user/config-el-file)
+    (byte-compile-file user/config-el-file)
+    (load-file user/config-el-file)))
 
 (user/reload-config)

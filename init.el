fb1b5c2c6e190322970c12166ce38e04b128c1b4
Caio Oliveira <caioaao@gmail.com>
2023-08-29 12:48:24 -0300

    move bootstrap to init.el + fix tangle noweb

--- init.el
+++ init.el
@@ -1,3 +1,25 @@
+;;; Bootstrapping
+;; setup straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+	(expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+      (url-retrieve-synchronously
+	"https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; setup use-package
+(straight-use-package 'use-package)
+
+;; setup org early to avoid version mismatch errors (https://www.reddit.com/r/emacs/comments/qcj33a/problem_and_workaround_with_orgmode_function/)
+(straight-use-package 'org)
+
+;;; Tangle and auto load literate config
 (defvar user/config-org-file (concat user-emacs-directory "config.org"))
 (defvar user/config-el-file  (concat user-emacs-directory "config.el"))
 

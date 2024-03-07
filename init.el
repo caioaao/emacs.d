666f7fe79d92bbb0bb2df5141127c5baa909aac0
Caio Oliveira <caioaao@gmail.com>
2024-03-07 18:51:18 -0300

    define magit fn that works in `current-project`

--- init.el
+++ init.el
@@ -40,3 +40,27 @@
     (my/install-all-language-grammars)))
 
 (user/reload-config)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((eval add-hook 'after-save-hook
+           (lambda nil
+             (if
+                 (y-or-n-p "Tangle?")
+                 (org-babel-tangle)))
+           nil t)
+     (eval add-hook 'after-save-hook
+           (lambda nil
+             (if
+                 (y-or-n-p "Reload?")
+                 (load-file user-init-file)))
+           nil t))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
